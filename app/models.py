f1f1d27ab54a58441cfb4eecfef69187ad1cb6ab
Peter Jones <peternjones95@gmail.com>
2017-10-05 20:08:44 -0400

    Add BetUsers

--- app/models.py
+++ app/models.py
@@ -12,6 +12,8 @@ class User(db.Model):
     email = db.Column(db.String(60), nullable=False)
     birthday = db.Column(db.DateTime, nullable=False)
 
+    bets_in = db.relationship('BetUsers', backref='user', lazy=True)
+
     def __init__(self, id, username, email, birthday):
         self.id = id
         self.username = username
@@ -44,6 +46,8 @@ class Bet(db.Model):
     amount = db.Column(db.Integer, nullable=False)
     completed = db.Column(db.Boolean, default=False, nullable=False)
 
+    bet_users = db.relationship('BetUsers', backref='bet', lazy=True)
+
     def __init__(self, max_users, title, text, amount):
         self.max_users = max_users
         self.title = title
@@ -60,6 +64,40 @@ class Bet(db.Model):
     @staticmethod
     def get_all():
         return Bet.query.all()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class BetUsers(db.Model):
+    """
+        Create a BetUsers table
+    """
+
+    __tablename__ = 'BetUsers'
+
+    bet_id = db.Column(db.Integer, db.ForeignKey('bet.id'),
+                       nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
+                       nullable=False)
+
+
+    def __init__(self, bet_id, user_id):
+        bet_id = bet_id
+        user_id = user_id
+
+    def __repr__(self):
+        return '<BetUsers id: {}>'.format(self.id)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        return BetUsers.query.all()
 
     def delete(self):
         db.session.delete(self)
