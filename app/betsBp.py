832830bad38d1d41c45719036c322b61eb82d02c
Peter Jones <peternjones95@gmail.com>
2017-10-05 20:21:03 -0400

    Begin mybets endpoint

--- app/betsBp.py
+++ app/betsBp.py
@@ -1,13 +1,14 @@
 from flask import request, jsonify, abort, Blueprint
 
-
+import requests
+import json
 from app import models
 
 betRoutes = Blueprint('betsBp', __name__)
 
 
 @betRoutes.route('/publicfeed', methods=['GET'])
-def hello_world():
+def public_feed():
     # GET
     bets = models.Bet.get_all()
     results = []
@@ -26,3 +27,9 @@ def hello_world():
     response = jsonify({'bets': results})
     response.status_code = 200
     return response
+
+@betRoutes.route('/mybets', methods=['POST'])
+def my_bets():
+    if request.method == 'POST':
+        payload = json.loads(request.data.decode())
+        token = payload['authToken']
