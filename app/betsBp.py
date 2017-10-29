19e56f02d84bd193552d4834edb00aab74af56bc
Peter Jones <peternjones95@gmail.com>
2017-10-29 18:02:24 -0400

    Add private feed

--- app/betsBp.py
+++ app/betsBp.py
@@ -121,10 +121,72 @@ def private_feed():
                 results.append(obj)
 
             # Friends Side 1
-            friends_one = models.Friend.query.filter_by(user_to=user.id, status=1)
+            friends_one = models.Friend.query.filter_by(user_to=user.id, status=1).all()
 
             for friend_one in friends_one:
-                models.Bet.query.filter_by(user_id=friend_one.user_from)
+                friend_one_bets = models.Bet.query.filter_by(user_id=friend_one.user_from).all()
+
+                for friend_one_bet in friend_one_bets:
+                    # Get like count
+                    count = models.Likes.query.filter_by(bet_id=friend_one_bet.id).count()
+
+                    # Get if the current user liked the bet
+                    like = models.Likes.query.filter_by(bet_id=friend_one_bet.id, user_id=user.id).count()
+
+                    if count is 1:
+                        liked = True
+                    else:
+                        liked = False
+
+                    # Make JSONObject to return
+                    obj = {
+                        'id': bet.id,
+                        'creator_id': bet.creator_id,
+                        'max_users': bet.max_users,
+                        'title': bet.title,
+                        'description': bet.description,
+                        'amount': bet.amount,
+                        'winner': bet.winner,
+                        'locked': bet.locked,
+                        'complete': bet.complete,
+                        'num_likes': count,
+                        'liked': liked
+                    }
+                    results.append(obj)
+
+            # Friends Side 2
+            friends_two = models.Friend.query.filter_by(user_from=user.id, status=1).all()
+
+            for friend_two in friends_two:
+                friend_two_bets = models.Bet.query.filter_by(user_id=friend_two.user_to).all()
+
+                for friend_two_bet in friend_two_bets:
+                    # Get like count
+                    count = models.Likes.query.filter_by(bet_id=friend_two_bet.id).count()
+
+                    # Get if the current user liked the bet
+                    like = models.Likes.query.filter_by(bet_id=friend_two_bet.id, user_id=user.id).count()
+
+                    if count is 1:
+                        liked = True
+                    else:
+                        liked = False
+
+                    # Make JSONObject to return
+                    obj = {
+                        'id': bet.id,
+                        'creator_id': bet.creator_id,
+                        'max_users': bet.max_users,
+                        'title': bet.title,
+                        'description': bet.description,
+                        'amount': bet.amount,
+                        'winner': bet.winner,
+                        'locked': bet.locked,
+                        'complete': bet.complete,
+                        'num_likes': count,
+                        'liked': liked
+                    }
+                    results.append(obj)
 
             response = jsonify({'bets': results})
             response.status_code = 200
@@ -134,6 +196,7 @@ def private_feed():
 ######## My Bets ########
 @betRoutes.route('/mybets', methods=['POST'])
 def my_bets():
+
     authClass = authBackend()
 
     if request.method == 'POST':
@@ -148,10 +211,12 @@ def my_bets():
             return jsonify({'result': False, 'error': 'Failed Token'}), 400
         else:
 
-            bets = models.Bet.query.filter_by(user_id=user.id).all()
+            bet_users = models.BetUsers.query.filter_by(user_id=user.id).all()
             results = []
 
-            for bet in bets:
+            for user in bet_users:
+
+
 
                 # Get like count
                 count = models.Likes.query.filter_by(bet_id=bet.id).count()
