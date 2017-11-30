73e041a6a82ef674d26410a243348787b9a1d641
Peter Jones <peternjones95@gmail.com>
2017-11-30 14:05:42 -0500

    Re fix models

--- app/models.py
+++ app/models.py
@@ -107,9 +107,9 @@ class Bet(db.Model):
     icon = db.Column(db.Integer, nullable=True)
 
     # One to Many
-    bet_users = db.relationship('BetUsers', backref='bet', lazy=True, cascade='all, delete-orphan')
-    likes = db.relationship('Likes', backref='bet', lazy=True, cascade='all, delete-orphan')
-    transactions = db.relationship('Transactions', backref='bet', lazy=True, cascade='all, delete-orphan')
+    #bet_users = db.relationship('BetUsers', backref='bet', lazy=True, cascade='all, delete-orphan')
+    #likes = db.relationship('Likes', backref='bet', lazy=True, cascade='all, delete-orphan')
+    #transactions = db.relationship('Transactions', backref='bet', lazy=True, cascade='all, delete-orphan')
 
     # Foreign Relationships
     creator_id = db.Column(db.Integer, db.ForeignKey('Users.id', ondelete='CASCADE'), nullable=False)
@@ -180,7 +180,7 @@ class BetUsers(db.Model):
 
     # Foreign Relationships
     bet_id = db.Column(db.Integer, db.ForeignKey('Bets.id', ondelete='CASCADE'), nullable=False)
-    bet_link = db.relationship('Bet', backref=db.backref('bet_bet_user', passive_deletes=True))
+    bet = db.relationship('Bet', backref=db.backref('bet_bet_user', passive_deletes=True))
 
     user_id = db.Column(db.Integer, db.ForeignKey('Users.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('user_bet_user', passive_deletes=True))
