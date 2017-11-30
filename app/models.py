ca718920170c3f79c15f9096d6b8826208e4b8e3
Peter Jones <peternjones95@gmail.com>
2017-11-30 14:02:22 -0500

    possible fix

--- app/models.py
+++ app/models.py
@@ -180,7 +180,7 @@ class BetUsers(db.Model):
 
     # Foreign Relationships
     bet_id = db.Column(db.Integer, db.ForeignKey('Bets.id', ondelete='CASCADE'), nullable=False)
-    bet = db.relationship('Bet', backref=db.backref('bet_bet_user', passive_deletes=True))
+    bet_link = db.relationship('Bet', backref=db.backref('bet_bet_user', passive_deletes=True))
 
     user_id = db.Column(db.Integer, db.ForeignKey('Users.id', ondelete='CASCADE'), nullable=False)
     user = db.relationship('User', backref=db.backref('user_bet_user', passive_deletes=True))
